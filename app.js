e55aa59818711af1357eccf4cf0a5e59415ae58a
Wang Jie <doudou19758@gmail.com>
2016-12-02 23:30:44 +0800

    slight progress

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-server.listen(80);
+var client = require('redis').createClient(6379, '127.0.0.1', {});
+
+server.listen(3000);
 
 app.get('/', function(req, res) {
   res.sendfile('views/index.html');
 });
 
-io.set('log leverl', 1);
-
-io.on('connection', function (socket) {
+io.on('connection', function(socket) {
   socket.emit('open');
+
+  socket.on('message')
 
   console.log(socket.handshake);
 });
